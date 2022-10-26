e8074681764d0bb52afa5c4e5cd09fdf300b5345
Yelessov Abilkaiyr <200103320@stu.sdu.edu.kz>
2022-10-26 15:37:23 +0600

    listener for get started button added

--- app/src/main/java/com/example/foodorderproject/MainActivity.kt
+++ app/src/main/java/com/example/foodorderproject/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.foodorderproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_page)
+        setContentView(R.layout.activity_main)
+        val getstarted: Button = findViewById(R.id.getstarted)
+        getstarted.setOnClickListener {
+            setContentView(R.layout.main_page)
+        }
     }
 }
